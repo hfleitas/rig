552824d65f2c556055d48b2671e082a8c4f38c63
Hiram Fleitas <hiramfleitas@hotmail.com>
2021-09-22 11:55:39 -0400

    Create SynapseAddPackage.ps1

---
+++ runbooks/SynapseAddPackage.ps1
@@ -0,0 +1,16 @@
+Param(
+    $workspace = "synapsehiram2",
+    $rg = "rig",
+    $pkg = "path/to/package.whl",
+    $pool = "hirampool"
+)
+
+# login
+$null = Connect-AzAccount -ServicePrincipal `
+    -Tenant 'yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyy' `
+    -ApplicationId '00000000-0000-0000-0000-00000000' `
+    -CertificateThumbprint 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+#pkg
+$newpkg = New-AzSynapseWorkspacePackage -WorkspaceName $workspace -Package $pkg
+Update-AzSynapseSparkPool -WorkspaceName $workspace -Name $pool -PackageAction Add -Package $package
