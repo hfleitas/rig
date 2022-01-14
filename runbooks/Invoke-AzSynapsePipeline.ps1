a5c7a6ffb8286ccb9f1cd961fd3726fd3b5fd639
Hiram Fleitas <hiramfleitas@hotmail.com>
2022-01-14 12:38:47 -0500

    Update Invoke-AzSynapsePipeline.ps1
    Updated to use Managed Identity and return RunId from Synapse

--- runbooks/Invoke-AzSynapsePipeline.ps1
+++ runbooks/Invoke-AzSynapsePipeline.ps1
@@ -1,15 +1,13 @@
 Param(
-    $workspace = "synapsehiram2",
-    $pipeline = "wait",
-    $rg = "rig"
+    $workspace = "wplushiramsynapse",
+    $pipeline = "wait"
 )
 
 # login
-$null = Connect-AzAccount -ServicePrincipal `
-    -Tenant 'yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyy' `
-    -ApplicationId '00000000-0000-0000-0000-00000000' `
-    -CertificateThumbprint 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+$null = Connect-AzAccount -Identity
 
-# run pipeline
-$ws = Get-AzSynapseWorkspace -Name $workspace -ResourceGroup $rg
-$ws | Invoke-AzSynapsePipeline -PipelineName $pipeline
+#run pipeline
+$RunId = Invoke-AzSynapsePipeline -WorkspaceName $workspace -PipelineName $pipeline
+$RunId 
+
+#Write-Host ($RunId | ConvertTo-Json)
