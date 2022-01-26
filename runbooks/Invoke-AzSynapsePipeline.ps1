456c9f9f5d024beb802ea0a44b57c1b477f24135
Hiram Fleitas <hiramfleitas@hotmail.com>
2022-01-26 10:30:04 -0500

    Update Invoke-AzSynapsePipeline.ps1

--- runbooks/Invoke-AzSynapsePipeline.ps1
+++ runbooks/Invoke-AzSynapsePipeline.ps1
@@ -1,19 +1,19 @@
 Param(
     $workspace = "wplushiramsynapse",
     $pipeline = "params",
-	$key = $null, #"p1"
-	$val = $null #5
+    $key = $null, #"p1"
+    $val = $null #5
 )
 
 # login
 $null = Connect-AzAccount -Identity
 
 #add to hashtable
-$p = @{}
-$p.Add($key,$val)
+$ht = @{}
+$ht.Add($key,$val)
 
 #run pipeline
-$RunId = Invoke-AzSynapsePipeline -WorkspaceName $workspace -PipelineName $pipeline -Parameter $p 
+$RunId = Invoke-AzSynapsePipeline -WorkspaceName $workspace -PipelineName $pipeline -Parameter $ht 
 $RunId 
 
 #Write-Host ($RunId | ConvertTo-Json)
